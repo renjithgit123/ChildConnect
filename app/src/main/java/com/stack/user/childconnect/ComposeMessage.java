56b93c6605eaef6dab7f190ac067abd1b2e122e0
renjithgit123 <renjith.mr@stackontech.com>
2016-08-26 11:35:00 +0530

    Childconnect init

--- app/src/main/java/com/stack/user/childconnect/ComposeMessage.java
+++ app/src/main/java/com/stack/user/childconnect/ComposeMessage.java
@@ -11,5 +11,6 @@ public class ComposeMessage extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.compose_message);
+
     }
 }
